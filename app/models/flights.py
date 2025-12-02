d5a62c9c9cf8c37e105a48232371a7fa6b2cc678
angelina77a <volobuyeva_07@bk.ru>
2025-12-02 10:33:15 +0300

    proekt

---
+++ app/models/flights.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+from datetime import datetime
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.database.database import Base
+
+if TYPE_CHECKING:
+    from app.models.users import UserModel
+
+
+class RoleModel(Base):
+    __tablename__ = "flights"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    price: Mapped[int] = mapped_column(float, default=0.0)  
+    date: Mapped[datetime] = mapped_column(String(255), nullable=False)
+    time: Mapped[datetime] = mapped_column(String(50), nullable=False)
+    plane: Mapped[int] = mapped_column(String(255), nullable=False)
+    paces: Mapped[int] = mapped_column(String(255), nullable=False)
+    booking_id: Mapped[int] = mapped_column(primary_key=True, nullable=True)
